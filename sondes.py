2480ad517daf8abc2813bf674c370bda2bc6196c
Geordie Millar <gm@stackunderflow.com>
2018-01-07 19:16:11 +1100

    use fastkml for sonde data

--- sondes.py
+++ sondes.py
@@ -2,87 +2,82 @@
 import time
 import json
 from collections import defaultdict
+import fastkml
+from shapely.geometry import Point, LineString
 import paho.mqtt.client
 
 sonde_tracks = defaultdict(list)
+ns = '{http://www.opengis.net/kml/2.2}'
 
-def kml(sonde_data):
-    pos_kml = """<StyleMap id="msn_track_%(id)s">
-        <Pair>
-            <key>normal</key>
-            <styleUrl>#sn_track_%(id)s</styleUrl>
-        </Pair>
-        <Pair>
-            <key>highlight</key>
-            <styleUrl>#sn_track_%(id)s</styleUrl>
-        </Pair>
-    </StyleMap>
-    <Style id="sn_track_%(id)s">
-        <IconStyle>
-            <scale>1.4</scale>
-            <Icon>
-                <href>http://maps.google.com/mapfiles/kml/shapes/track.png</href>
-            </Icon>
-            <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
-            <heading>%(heading).1f</heading>
-        </IconStyle>
-        <ListStyle>
-        </ListStyle>
-    </Style>
-    <Placemark id="point">
-        <name>%(type)s %(id)s</name>
-        <LookAt>
-            <latitude>%(lat).6f</latitude>
-            <longitude>%(lon).6f</longitude>
-            <altitude>0</altitude>
-            <heading>0</heading>
-            <tilt>0</tilt>
-            <range>1000</range>
-        </LookAt>
-        <styleUrl>#sn_track_%(id)s</styleUrl>
-        <Point>
-            <coordinates>%(lon).6f,%(lat).6f,%(alt).1f</coordinates>
-            <altitudeMode>absolute</altitudeMode>
-        </Point>
-    </Placemark>
-""" % sonde_data[-1]
+def pos_kml(last_sonde_data):
+    sonde_icon_style = fastkml.styles.IconStyle(
+        ns=ns, 
+        icon_href="http://maps.google.com/mapfiles/kml/shapes/track.png", 
+        scale=2.0,
+        heading=last_sonde_data['heading'])
 
-    track_kml = """<Style id="s_line">
-        <LineStyle>
-            <color>ffff8000</color>
-            <width>3</width>
-        </LineStyle>
-    </Style>
-    <Placemark>
-        <name>Untitled Path</name>
-        <styleUrl>#s_line</styleUrl>
-        <LineString>
-            <extrude>1</extrude>
-            <tessellate>1</tessellate>
-            <altitudeMode>absolute</altitudeMode>
-            <coordinates>
-            %s
-            </coordinates>
-        </LineString>
-    </Placemark>
-    """ % " ".join(["%(lon).6f,%(lat).6f,%(alt).1f" % loc for loc in sonde_data])
+    sonde_style = fastkml.styles.Style(
+        ns=ns,
+        styles=[sonde_icon_style])
 
-    return "%s\n%s" % (pos_kml, track_kml)
+    sonde_placemark = fastkml.kml.Placemark(
+        ns=ns, 
+        id=last_sonde_data['id'],
+        name=last_sonde_data['id'],
+        description="%(type)s %(id)s" % last_sonde_data,
+        styles=[sonde_style])
+
+    sonde_placemark.geometry = fastkml.geometry.Geometry(
+        ns=ns,
+        geometry=Point(last_sonde_data['lon'], last_sonde_data['lat'], last_sonde_data['alt']),
+        altitude_mode='absolute')
+
+    return sonde_placemark
+
+def track_kml(sonde_data):
+    sonde_track_line_style = fastkml.styles.LineStyle(
+        ns=ns,
+        color="ffff8000",
+        width=3.0)
+
+    sonde_track_style = fastkml.styles.Style(
+        ns=ns,
+        styles=[sonde_track_line_style])
+
+    sonde_line = fastkml.kml.Placemark(
+        ns=ns,
+        id=sonde_data[-1]['id'],
+        name="%(type)s %(id)s" % sonde_data[-1],
+        styles=[sonde_track_style])
+
+    line_data = [(loc['lon'], loc['lat'], loc['alt']) for loc in sonde_data]
+    if len(line_data) == 1:
+        line_data.append(line_data[0])
+
+    sonde_line.geometry = fastkml.geometry.Geometry(
+        ns=ns,
+        geometry=LineString(line_data),
+        altitude_mode='absolute',
+        extrude=True,
+        tessellate=True)
+
+    return sonde_line
 
 def write_kmls():
-    sonde_kmls = [kml(sonde_tracks[sonde]) for sonde in sonde_tracks.keys()]
+    kml_root = fastkml.kml.KML()
 
-    kml_data = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
-<Document>
-    <name>Chase Car.kml</name>
-    %s
-</Document>
-</kml>
-""" % "\n".join(sonde_kmls)
+    kml_doc = fastkml.kml.Document(
+        ns=ns,
+        name="Sonde Data")
+
+    for sonde in sonde_tracks.keys():
+        kml_doc.append(pos_kml(sonde_tracks[sonde][-1]))
+        kml_doc.append(track_kml(sonde_tracks[sonde]))
+
+    kml_root.append(kml_doc)
 
     with open("/tmp/sonde_loc.kml", 'w') as kml_file:
-        kml_file.write(kml_data)
+        kml_file.write(kml_doc.to_string())
 
 
 def on_message(client, userdata, message):
